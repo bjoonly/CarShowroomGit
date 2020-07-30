225ce210ea36a85cabf2979fc8f065d5b92384d4
bjoonly <elumnaya@gmail.com>
2020-07-30 17:55:03 +0300

    Create ctor for Car and add setters

--- CarShowroom/CarShowroom/CarShowroom.cpp
+++ CarShowroom/CarShowroom/CarShowroom.cpp
@@ -1,1 +1,28 @@
 #include "CarShowroom.h"
+Car::Car(string model, int year, double price, double volume) :model(model) {
+	SetYear(year);
+	SetPrice(price);
+	SetVolume(volume);
+}
+
+void Car::SetYear(int year) {
+	if (year < 1885)
+		year = 1885;
+	this->year = year;
+}
+
+void Car::SetModel(string model) {
+	this->model = model;
+}
+
+void Car::SetPrice(double price) {
+	if (price < 1)
+		price = 1;
+	this->price = price;
+}
+
+void Car::SetVolume(double volume) {
+	if (volume < 1)
+		volume = 1;
+	this->volume = volume;
+}
