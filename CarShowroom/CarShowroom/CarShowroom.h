0d60bbe91701f719ae6e38593a3bee0c4324370a
bjoonly <elumnaya@gmail.com>
2020-07-30 17:51:38 +0300

    Add struct Car

---
+++ CarShowroom/CarShowroom/CarShowroom.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<vector>
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+struct Car {
+	string model;
+	int year;
+	double price;
+	double volume;
+};
+
+
